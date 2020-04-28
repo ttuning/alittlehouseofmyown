4dac7b5b38ee67a2ee16cbe995f2651f4b4b5732
Tom Tuning <Thomas.E.Tuning@gmail.com>
2020-04-28 14:56:16 -0400

    Update start-esp-pub-fs-adapter.bat

--- start-esp-pub-fs-adapter.bat
+++ start-esp-pub-fs-adapter.bat
@@ -1,3 +1,3 @@
 
-$BPATH/dfesp_fs_adapter -C type=pub,url="dfESP://iotdemo.na.sas.com:61001/kafka1/cq1/kafka_incoming",fsname=/tmp/dstat_in.csv,fstype=csv,dateformat='%Y-%m-%d %H:%M:%S',header=2,growinginputfile=true,loglevel=trace,transport=kafka,transportconfigfile=/home/sas/kafka/kafkapub.cfg
+"C:\Program Files\SAS\Viya\SASEventStreamProcessingEngine\6.2\bin\dfesp_pi_adapter.exe"  -C type=pub,url="dfESP://GPSASDVESP01_62001.datainjestion.cq1.kafka_incoming",fsname=/tmp/dstat_in.csv,fstype=csv,dateformat='%Y-%m-%d %H:%M:%S',header=2,growinginputfile=true,loglevel=trace,transport=kafka,transportconfigfile=/home/sas/kafka/kafkapub.cfg
 
