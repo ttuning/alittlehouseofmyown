44ee50dcbbe9ac04cc5c13ffcc83c3ce09a87e3a
Tom Tuning <Thomas.E.Tuning@gmail.com>
2020-04-28 10:45:25 -0400

    Update list_topics.sh

--- list_topics.sh
+++ list_topics.sh
@@ -2,5 +2,8 @@
 
 #Copyright © 2019, SAS Institute Inc., Cary, NC, USA.  All Rights Reserved.
 #SPDX-License-Identifier: Apache-2.0
+BPATH=/home/tom.tuning@gapac.com/kafka/bin
 
-/opt/kafka/bin/kafka-topics.sh --list --zookeeper localhost:2181
+$BPATH/kafka-topics.sh --list \
+--zookeeper  z-1.edaviyaespprocessing.9mwv7t.c6.kafka.us-east-1.amazonaws.com:2181 
+
