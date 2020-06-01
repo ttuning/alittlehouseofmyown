e57d9234ca5cd3516389ada3f97bc664f6d9b7d3
Tom Tuning <Thomas.E.Tuning@gmail.com>
2020-06-01 11:19:12 -0400

    Update create-replicated-topicsL.sh

--- create-replicated-topicsL.sh
+++ create-replicated-topicsL.sh
@@ -42,4 +42,10 @@ $BPATH/kafka-topics.sh --create \
 --partitions 1 \
 --topic level3_62001.atom_datareturn.cq1.kafka_outgoing.O
 
+$BPATH/kafka-topics.sh --create \
+--zookeeper  z-1.edaviyaespprocessing.9mwv7t.c6.kafka.us-east-1.amazonaws.com:2181 \
+--replication-factor 3  \
+--partitions 1 \
+--topic SASESPAdapterMetapub02D7
+
 
